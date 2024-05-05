3c89ee0810a046ebb2bbefaf34d75aade0463592
girubato <76228466+girubato@users.noreply.github.com>
2024-05-05 15:25:21 -0400

    add react-simple-maps

---
+++ speciesdle/src/map.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+} from 'react-simple-maps';
+
+const map = ({ animal, shapefile }) => {
+    const [guess, setGuess] = useState('');
+    const [isCorrect, setIsCorrect] = useState(null);
+    // stuff
+  };
